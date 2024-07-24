3d50cac9760706e909b23370a0454cfdcfc44f21
Muh-Hassk <Muh.alkhuzaee@gmail.com>
2024-07-24 12:26:09 +0300

    New Scafolding

---
+++ TrainStationTracker.API/Data/Contactuspage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrainStationTracker.API.Data
+{
+    public partial class Contactuspage
+    {
+        public decimal Id { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+    }
+}
